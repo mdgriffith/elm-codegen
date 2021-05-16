f09c930fe878ff8e8f6fb213bdd7b03e96d54b29
Matthew Griffith <mdg.griffith@gmail.com>
2021-05-15 20:46:29 -0400

    bigger example in the playground

--- playground/src/Gen.elm
+++ playground/src/Gen.elm
@@ -1,11 +1,10 @@
 module Gen exposing (main)
 
-{-|-}
-
+{-| -}
 
 import Elm
-import Elm.Type as Type
 import Elm.Pattern as Pattern
+import Elm.Type as Type
 import Generate
 
 
@@ -21,14 +20,31 @@ main =
                 )
         , update =
             \msg model ->
-                (model, Cmd.none)
+                ( model, Cmd.none )
         , subscriptions = \_ -> Sub.none
         }
-
 
 
 file =
     Elm.file (Elm.moduleName [ "My", "Module" ])
         [ Elm.declaration "placeholder"
-            (Elm.string "a fancy string!")
+            (Elm.valueFrom (Elm.moduleAs [ "Json", "Decode" ] "Json")
+                "map2"
+            )
+        , Elm.declaration "myRecord"
+            (Elm.record
+                [ ( "field1", Elm.string "My cool string" )
+                , ( "field2", Elm.int 5 )
+                , ( "field4", Elm.string "My cool string" )
+                , ( "field5", Elm.int 5 )
+                , ( "field6", Elm.string "My cool string" )
+                , ( "field7"
+                  , Elm.record
+                        [ ( "field1", Elm.string "My cool string" )
+                        , ( "field2", Elm.int 5 )
+                        ]
+                  )
+                ]
+            )
+            |> Elm.expose
         ]
