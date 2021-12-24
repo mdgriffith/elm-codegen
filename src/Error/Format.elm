a038d64cf22a744a944b65d70c76152668efaaae
Matthew Griffith <mdg.griffith@gmail.com>
2021-12-24 11:25:20 -0500

    doc comments to Error.Format

--- src/Error/Format.elm
+++ src/Error/Format.elm
@@ -26,31 +26,37 @@ block lines =
 -}
 
 
+{-| -}
 cyan : String -> String
 cyan str =
     color 36 39 str
 
 
+{-| -}
 yellow : String -> String
 yellow str =
     color 33 39 str
 
 
+{-| -}
 green : String -> String
 green str =
     color 32 39 str
 
 
+{-| -}
 red : String -> String
 red str =
     color 31 39 str
 
 
+{-| -}
 grey : String -> String
 grey str =
     color 90 39 str
 
 
+{-| -}
 color : Int -> Int -> String -> String
 color openCode closeCode content =
     let
