2b045a1831ff95d5bd5d81f7b93e2d0d5133dfdf
Leonardo Taglialegne <cmt.miniBill@gmail.com>
2022-07-22 08:40:00 +0200

    Better template for Gen.CodeGen.Generate.elm

--- cli/templates/CodeGen.Generate.elm.ts
+++ cli/templates/CodeGen.Generate.elm.ts
@@ -1,91 +1,169 @@
 export default () =>
   `
-port module Gen.CodeGen.Generate exposing (run, File, error, files, info)
+port module Gen.CodeGen.Generate exposing
+    ( noInput, simple, simpleFrom
+    , run
+    , File
+    , error, files, info
+    )
 
+{-|
+
+
+# Simple API
+
+@docs noInput, simple, simpleFrom
+
+
+# With errors
+
+@docs run, Error
+
+
+# Types
+
+@docs File
+
+
+# Flexible API
+
+This is the low level API if you need more control over the generation process proceeds.
+
+@docs error, files, info
+
+-}
+
+import Json.Decode exposing (Decoder)
 import Json.Encode as Json
 
 
-run : (flags -> List File) -> Program flags () ()
-run f =
-     Platform.worker
-          { init =
-               \\flags ->
-                    ( ()
-                    , files (f flags)
-                    )
-          , update =
-               \\_ model ->
-                    ( model, Cmd.none )
-          , subscriptions = \\_ -> Sub.none
-          }
+{-| Produce a list of files without taking input.
+-}
+noInput : List File -> Program flags () ()
+noInput f =
+    Platform.worker
+        { init =
+            \\_ ->
+                ( ()
+                , files f
+                )
+        , update =
+            \\_ model ->
+                ( model, Cmd.none )
+        , subscriptions = \\_ -> Sub.none
+        }
+
+
+{-| Given the flags, decoded from JSON, produce a list of files as output.
+-}
+simple : Decoder flags -> (flags -> List File) -> Program Json.Value () ()
+simple decoder f =
+    Platform.worker
+        { init =
+            \\flags ->
+                case Json.Decode.decodeValue decoder flags of
+                    Ok input ->
+                        ( ()
+                        , files (f input)
+                        )
+
+                    Err e ->
+                        ( ()
+                        , error
+                            [ { title = "Error decoding flags"
+                              , description = Json.Decode.errorToString e
+                              }
+                            ]
+                        )
+        , update =
+            \\_ model ->
+                ( model, Cmd.none )
+        , subscriptions = \\_ -> Sub.none
+        }
+
+
+{-| Given the file passed in with \`--flags-from\` as String, produce a list of files as output.
+-}
+simpleFrom : (String -> List File) -> Program String () ()
+simpleFrom f =
+    Platform.worker
+        { init =
+            \\flags ->
+                ( ()
+                , files (f flags)
+                )
+        , update =
+            \\_ model ->
+                ( model, Cmd.none )
+        , subscriptions = \\_ -> Sub.none
+        }
 
 
 type alias File =
-     { path : String
-     , contents : String
-     }
+    { path : String
+    , contents : String
+    }
 
 
-encodeFile : File -> Json.Value
-encodeFile file =
-     Json.object
-          [ ( "path", Json.string file.path )
-          , ( "contents", Json.string file.contents )
-          ]
+type alias Error =
+    { title : String
+    , description : String
+    }
 
 
-{-|
+run :
+    (flags
+     -> Result (List Error) { info : List String, files : List File }
+    )
+    -> Program flags () ()
+run f =
+    Platform.worker
+        { init =
+            \\flags ->
+                ( ()
+                , case f flags of
+                    Ok result ->
+                        Cmd.batch <|
+                            List.map info result.info
+                                ++ [ files result.files ]
 
-     Provide the list of files to be generated.
-     These files will be generated and the script will end.
+                    Err errors ->
+                        error errors
+                )
+        , update =
+            \\_ model ->
+                ( model, Cmd.none )
+        , subscriptions = \\_ -> Sub.none
+        }
 
+
+{-| Provide the list of files to be generated.
+These files will be generated and the script will end.
 -}
 files : List File -> Cmd msg
 files list =
-     onSuccessSend (List.map encodeFile list)
+    onSuccessSend list
 
 
-{-|
-
-     Report an error.  The script will end.
-
+{-| Report an error. The script will end.
 -}
-error :
-     List
-     { title : String
-     , description : String
-     }
-     -> Cmd msg
+error : List Error -> Cmd msg
 error errs =
-     onFailureSend (List.map encodeError errs)
-
-
-encodeError :
-     { title : String
-     , description : String
-     }
-     -> Json.Value
-encodeError err =
-     Json.object
-          [ ( "title", Json.string err.title )
-          , ( "description", Json.string err.description )
-          ]
+    onFailureSend errs
 
 
 {-| Report some info. The script will continue to run.
 -}
 info : String -> Cmd msg
 info err =
-     onInfoSend err
+    onInfoSend err
 
 
-port onSuccessSend : List Json.Value -> Cmd msg
+port onSuccessSend : List File -> Cmd msg
 
 
-port onFailureSend : List Json.Value -> Cmd msg
+port onFailureSend : List Error -> Cmd msg
 
 
 port onInfoSend : String -> Cmd msg
-
-
 `.trimLeft()
