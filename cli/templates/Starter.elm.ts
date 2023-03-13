252a3ca476b9338c471ff2602ab257fe19b36276
Matthew Griffith <mdg.griffith@gmail.com>
2023-03-12 23:41:40 -0400

    Remove helper from the file generated by init

--- cli/templates/Starter.elm.ts
+++ cli/templates/Starter.elm.ts
@@ -6,7 +6,6 @@ module Generate exposing (main)
 import Elm
 import Elm.Annotation as Type
 import Gen.CodeGen.Generate as Generate
-import Gen.Helper
 
 
 main : Program {} () ()
@@ -16,18 +15,11 @@ main =
         ]
 
 
-
 file : Elm.File
 file =
     Elm.file [ "HelloWorld" ]
         [ Elm.declaration "hello"
             (Elm.string "World!")
-
-        -- Here's an example of using a helper file!
-        -- Add functions to codegen/helpers/{Whatever}.elm
-        -- run elm-codegen install
-        -- Then you can call those functions using import Gen.{Whatever}
-        , Elm.declaration "usingAHelper"
-            (Gen.Helper.add5 20)
         ]
+
 `
