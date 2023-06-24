03f28db6465630e2abfb1c2bdbe2e181a058a122
Leonardo Taglialegne <cmt.miniBill@gmail.com>
2023-06-24 18:15:55 +0200

    Add support for directories as --flagsFrom

--- cli/templates/CodeGen.Generate.elm.ts
+++ cli/templates/CodeGen.Generate.elm.ts
@@ -1,9 +1,9 @@
 export default () =>
   `
 port module Gen.CodeGen.Generate exposing
-    ( run, fromJson, fromText
+    ( run, fromJson, fromText, fromDirectory
     , withFeedback, Error
-    , File
+    , File, Directory(..)
     , error, files, info
     )
 
@@ -12,7 +12,7 @@ port module Gen.CodeGen.Generate exposing
 
 # Simple API
 
-@docs run, fromJson, fromText
+@docs run, fromJson, fromText, fromDirectory
 
 
 # With errors
@@ -22,7 +22,7 @@ port module Gen.CodeGen.Generate exposing
 
 # Types
 
-@docs File
+@docs File, Directory
 
 
 # Flexible API
@@ -33,6 +33,7 @@ This is the low level API if you need more control over the generation process p
 
 -}
 
+import Dict exposing (Dict)
 import Json.Decode exposing (Decoder)
 import Json.Encode as Json
 
@@ -55,7 +56,6 @@ run f =
 
 
 {-| Given the flags, decoded from JSON, produce a list of files as output.
-
 -}
 fromJson : Decoder flags -> (flags -> List File) -> Program Json.Value () ()
 fromJson decoder f =
@@ -97,6 +97,74 @@ fromText f =
             \\_ model ->
                 ( model, Cmd.none )
         , subscriptions = \\_ -> Sub.none
+        }
+
+
+{-| Given the directory passed in with \`--flags-from\` as a file tree, produce a list of files as output.
+-}
+fromDirectory : (Directory -> List File) -> Program Json.Value () ()
+fromDirectory f =
+    Platform.worker
+        { init =
+            \\flags ->
+                case Json.Decode.decodeValue directoryDecoder flags of
+                    Ok input ->
+                        ( ()
+                        , files (f input)
+                        )
+
+                    Err e ->
+                        ( ()
+                        , error
+                            [ { title = "Error decoding flags"
+                              , description = Json.Decode.errorToString e
+                              }
+                            ]
+                        )
+        , update =
+            \\_ model ->
+                ( model, Cmd.none )
+        , subscriptions = \\_ -> Sub.none
+        }
+
+
+directoryDecoder : Decoder Directory
+directoryDecoder =
+    Json.Decode.lazy
+        (\\_ ->
+            Json.Decode.oneOf
+                [ Json.Decode.map Ok Json.Decode.string
+                , Json.Decode.map Err directoryDecoder
+                ]
+                |> Json.Decode.dict
+                |> Json.Decode.map
+                    (\\entries ->
+                        entries
+                            |> Dict.toList
+                            |> List.foldl
+                                (\\( name, entry ) ( dirAcc, fileAcc ) ->
+                                    case entry of
+                                        Ok file ->
+                                            ( dirAcc, ( name, file ) :: fileAcc )
+
+                                        Err directory ->
+                                            ( ( name, directory ) :: dirAcc, fileAcc )
+                                )
+                                ( [], [] )
+                            |> (\\( dirAcc, fileAcc ) ->
+                                    Directory
+                                        { directories = Dict.fromList dirAcc
+                                        , files = Dict.fromList fileAcc
+                                        }
+                               )
+                    )
+        )
+
+
+type Directory
+    = Directory
+        { files : Dict String String
+        , directories : Dict String Directory
         }
 
 
