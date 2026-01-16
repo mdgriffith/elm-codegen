55d5edab4656593bedb2bd83dffd8c68b3a62743
Leonardo Taglialegne <leonardo@taglialegne.it>
2026-01-16 18:09:38 +0100

    Add test for redundant modules in imports

---
+++ tests/RedundantQualification.elm
@@ -0,0 +1,54 @@
+module RedundantQualification exposing (suite)
+
+import Elm
+import Elm.Annotation
+import Elm.Arg
+import Elm.Expect
+import Test exposing (Test, describe, test)
+
+
+suite : Test
+suite =
+    describe "Avoid redundant imports"
+        [ inTypeAliases
+        , inAnnotation
+        ]
+
+
+inTypeAliases : Test
+inTypeAliases =
+    test "In type aliases" <|
+        \_ ->
+            Elm.Expect.fileContentAs
+                (Elm.file [ "Foo" ]
+                    [ Elm.alias "Bar" (Elm.Annotation.named [ "Foo" ] "Baz") ]
+                )
+                """
+module Foo exposing (..)
+
+
+type alias Bar =
+    Baz
+"""
+
+
+inAnnotation : Test
+inAnnotation =
+    test "In annotation" <|
+        \_ ->
+            Elm.Expect.fileContentAs
+                (Elm.file [ "Foo" ]
+                    [ Elm.fn
+                        (Elm.Arg.varWith "v" (Elm.Annotation.named [ "Foo" ] "Baz"))
+                        identity
+                        |> Elm.declaration "id"
+                    ]
+                )
+                """
+module Foo exposing (..)
+
+
+id : Baz -> Baz
+id v =
+    v
+"""
