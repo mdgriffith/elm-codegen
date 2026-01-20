89068d5e0791f84b2fa9b7b5c7ecc530c6170665
Leonardo Taglialegne <leonardo@taglialegne.it>
2026-01-20 12:27:58 +0100

    Add Elm.Declare.exposeConstructor

--- src/Elm/Declare.elm
+++ src/Elm/Declare.elm
@@ -5,7 +5,7 @@ module Elm.Declare exposing
     , Value, value
     , function
     , Module, module_, with, withUnexposed
-    , Annotation, alias, customType
+    , Annotation, alias, customType, exposeConstructor
     , toFile, include, withSubmodule
     , customTypeAdvanced, CustomType
     , variant0, variant1, variant2, variant3, variant4
@@ -109,7 +109,7 @@ And handle the imports and everything.
 
 @docs Module, module_, with, withUnexposed
 
-@docs Annotation, alias, customType
+@docs Annotation, alias, customType, exposeConstructor
 
 @docs toFile, include, withSubmodule
 
@@ -259,6 +259,13 @@ customType name variants =
     }
 
 
+{-| Expose the constructors of a custom type.
+-}
+exposeConstructor : Annotation -> Annotation
+exposeConstructor unexposed =
+    { unexposed | declaration = Elm.exposeConstructor unexposed.declaration }
+
+
 {-| -}
 type CustomTypeBuilder case_ make_
     = CustomTypeBuilder
@@ -273,9 +280,9 @@ type CustomTypeBuilder case_ make_
 
 {-| -}
 customTypeAdvanced : String -> { exposeConstructor : Bool } -> make_ -> CustomTypeBuilder case_ make_
-customTypeAdvanced name { exposeConstructor } make_ =
+customTypeAdvanced name options make_ =
     CustomTypeBuilder
-        { exposeConstructor = exposeConstructor
+        { exposeConstructor = options.exposeConstructor
         , name = name
         , variants = []
         , make_ = make_
